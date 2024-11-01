f598f03c653a6598fa34d62675b34202dc90d43c
Daniel Velez <59095877+urfavcontrarian@users.noreply.github.com>
2024-11-01 15:50:18 -0700

    Added Manhattan distance calculator, change state swap functions to one streamlined function. Added parent pointer to Node class.

--- main.cpp
+++ main.cpp
@@ -1,58 +1,80 @@
 #include <vector>
 #include <iostream>
 #include <queue>
+#include <cmath>
 #include <unordered_set>
 #include <functional>
 
 using namespace std;
 
 struct Node {
-    std::vector<std::vector<int>> state;  // 3x3 puzzle board
+    vector<vector<int>> state;  // 3x3 puzzle board
     // example input --> vector<vector<int>> puzzle
     //                    = {{1, 0, 3}, {4, 2, 6}, {7, 5, 8}};
     int Gcost;                 // Cost from the start node
-    // int Hcost;              // We'll use this later 
-    Node* left;
-    Node* right; 
-    Node* down;
-    Node* up;
+    int Hcost;              // We'll use this later 
+    Node* parent;
 
-    Node(std::vector<std::vector<int>> puzzle, int g = 0, Node* leftNode = nullptr, Node* rightNode = nullptr, Node* downNode = nullptr, Node* upNode = nullptr)
-        : state(puzzle), Gcost(g), left(leftNode), right(rightNode), down(downNode), up(upNode) {}
-
+    Node(vector<vector<int>> puzzle, int g = 0)
+        : state(puzzle), Gcost(g), Hcost(h), parent(p) {}
     // Comparator for priority queue (for UCS, no heuristic so just Gcost)
-    bool operator>(const Node& other) const {
-        return Gcost > other.Gcost;
+    int f(){
+        return Gcost + Hcost;
     }
 };
 
 class Problem {
 public:
-    std::vector<std::vector<int>> initialState;
-    std::vector<std::vector<int>> goalState;
+    vector<vector<int>> initialState;
+    vector<vector<int>> goalState;
 
-    Problem(std::vector<std::vector<int>> init, std::vector<std::vector<int>> goal)
+    Problem(vector<vector<int>> init, vector<vector<int>> goal)
         : initialState(init), goalState(goal) {}
 
     // Check if the current state is the goal state
-    bool isGoal(const std::vector<std::vector<int>>& state) const {
+    bool isGoal(const vector<vector<int>>& state) const {
         return state == goalState;
     }
 
+    int h_euclid_dist(vector<vector<int>> state, vector<vector<int>> goal){
+        int dist = 0;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                int val = state[i][j];
+                if val != 0 {
+                    for(int k = 0; k < 3; k++){
+                        for(int l = 0; l < 3; l++){
+                            if(goal[k][l] == val){
+                                dist += abs(i-k) + abs(j-l);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return distance;
+    }
+
     // Find blank space position in the puzzle
-    std::pair<int, int> findBlank(const std::vector<std::vector<int>>& state) const {
+    pair<int, int> findBlank(const vector<vector<int>>& state) const {
         for (int i = 0; i < 3; ++i)
             for (int j = 0; j < 3; ++j)
                 if (state[i][j] == 0) return {i, j};
 
-        std::cout << std::endl<< "ERROR, blank space does not exist" << std::endl;
+        cout << endl<< "ERROR, blank space does not exist" << endl;
         return {-1, -1}; // Should never reach here if input is valid
     }
 
-    vector<vector<int>> SwapState (pair<int, int> ) {}
+    vector<vector<int>> SwapState (vector<vector<int>> state, int x1, int y1, int x2, int y2) {
+        int temp = state[x2][y2]
+        state[x2][y2] = state[x1][y2];
+        state[x1][y1] = temp;
+        return state;
+    };
 
 
-    vector<vector<int>> SwapUp (const vector<vector<int>>& constState, pair<int, int> current) {
+   /* vector<vector<int>> SwapUp (const vector<vector<int>>& constState, pair<int, int> current) {
         vector<vector<int>> state = constState;
         int currY = current.first;
         int currX = current.second;
@@ -128,7 +150,7 @@ public:
         
         return state;
 
-    }
+    } */
 
     void Moves(Node& currentNode) {
 
@@ -152,25 +174,25 @@ public:
 
             // currentNode->up =
             Node upNode = currentNode; 
-            upNode.state = SwapUp(currentState, ghost);
+            upNode.state = swapState(upNode.state, ghost.first, ghost.second, ghost.first - 1, ghost.second);
+            upNode->parent = currentNode;
         }
         if(ghost.first != 2) {
             Node downNode = currentNode;
-            downNode.state = SwapDown(currentState, ghost);
+            downNode.state = swapState(downNode.state, ghost.first, ghost.second, ghost.first + 1, ghost.second);
+            downNode->parent = currentNode;
         }
         if(ghost.second != 0) {
             Node leftNode = currentNode;
-            leftNode.state = SwapLeft(currentState, ghost);
+            leftNode.state = swapState(leftNode.state, ghost.first, ghost.second, ghost.first, ghost.second - 1);
+            leftNode->parent = currentNode;
+            
         }
         if(ghost.second != 2) {
             Node rightNode = currentNode;
-            rightNode.state = SwapRight(currentState, ghost);
+            rightNode.state = swapState(rightNode.state, ghost.first, ghost.second, ghost.first, ghost.second + 1);
+            rightNode->parent = currentNode;
         }
-
-
-
-
-
     }
 
     // 1 0 3
@@ -178,6 +200,25 @@ public:
     // 7 5 8
 
 };
+
+vector<vector<int>> createPuzzle(){
+    
+}
+
+void menu(){
+    Const vector<vector<int>> Goal
+    string in = "";
+    while (in != "3"){
+        cout << "Hello! Welcome to the 8 puzzle generator! Please input either:" << endl;
+        "1. For a default puzzle" << endl << "2. For a custom puzzle" << endl << "or 3. To quit";
+        cin >> in;
+        if(in == 1){
+            vector<vector<int>> defaultPuzz{{1,0,3},{4,2,6},{7,5,8}};
+            Node defaultNode(puzzle = defaultPuzz);
+        }
+    }
+}
+
 
 
 
